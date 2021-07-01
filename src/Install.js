2a7dbcdf9762cc089ec6f4b00f75f20811ebe25d
Adrian Grimm <usmcamgrimm@gmail.com>
2021-07-01 18:45:59 -0400

    Fix existing pages and tweak the css a bit

--- src/Install.js
+++ src/Install.js
@@ -1,15 +1,13 @@
-import logo from './resources/logo.png';
 import './App.css';
 
 function Install() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="ses-welcome">
         <p>
           Install Checklist
         </p>
-      </header>
+      </div>
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,13 @@
-import logo from './resources/logo.png';
 import './App.css';
 
 function Login() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="ses-welcome">
         <p>
           Log In
         </p>
-      </header>
+      </div>
     </div>
   );
 }
