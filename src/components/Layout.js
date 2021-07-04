dc8f6c5222e8ca87338e8cff53f76e6caf5a96ba
Adrian Grimm <usmcamgrimm@gmail.com>
2021-07-04 13:48:07 -0400

    Working on a default global background?

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import { Global, css } from '@emotion/react'
+import styled from '@emotion/styled'
+
+const Wrapper = styled.div`
+  background: #031f30;
+  font-family: sans-serif;
+`
+
 export default function Layout({ children }) {
   return (
     <>
@@ -9,9 +17,16 @@ export default function Layout({ children }) {
         <Link to='/install'>Install</Link>
         <Link to='/appData'>Data</Link>
       </nav>
-      <main>
-        {children}
-      </main>
+      <Wrapper>
+        <Global
+          styles={css`
+            div {
+              background: #031f30;
+            }
+          `}
+        />
+        { children }
+      </Wrapper>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import Layout from '../components/Layout'
 
+import styled from '@emotion/styled'
+import { css } from '@emotion/react'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
 export default function Index() {
   return (
     <Layout>
-      <h1>
-        Securitas Electronic Security
-      </h1>
-      <h2>Managed Assets Portal</h2>
+      <Container>
+        <h1>
+          Securitas Electronic Security
+        </h1>
+        <h2>
+          Managed Assets Portal
+        </h2>
+      </Container>
     </Layout>
   )
 }
